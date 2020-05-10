5b1869a1194315841a63d4aa1a5f5fc79cd33344
Lougans de Matos <lougans.lougans99@gmail.com>
2020-05-10 05:10:35 -0300

    Update

--- src/js/call.js
+++ src/js/call.js
@@ -1,7 +1,6 @@
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register('./src/js/sw.js', { scope: '/' }).then(function(reg) {
+  navigator.serviceWorker.register('./src/js/sw.js').then(function(reg) {
       console.log('[Companion]', 'Service worker foi registrado!');
-      console.log(reg);
   });
 }
 
